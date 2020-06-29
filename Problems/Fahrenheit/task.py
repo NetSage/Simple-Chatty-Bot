b48c30156616a069a880e5b1779612057665fcf0
Alexander Smith <alexsmith@purelymail.com>
2020-06-29 11:08:41 -0500

    Initial commit

---
+++ Problems/Fahrenheit/task.py
@@ -0,0 +1,3 @@
+def fahrenheit_to_celsius(temp):
+    new_temp = (temp - 32) * (5 / 9)
+    return round(new_temp, 3)
---
+++ Problems/Longest word/task.py
@@ -0,0 +1,8 @@
+word1 = input()
+word2 = input()
+
+# How many letters does the longest word contain?
+letters1 = len(word1)
+letters2 = len(word2)
+
+print(max(letters1, letters2))
